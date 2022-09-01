ae2347dafd93ea3b7e9ff55a69bac74a98ef7729
Togarci <thyago.garcia10@gmail.com>
2022-09-01 09:33:57 -0300

    correcao titulo

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      
+      <h1 className="text-lime-700 text-2xl font-bold">
+        Hello world!
+      </h1>
+
+    </div>
+  )
+}
+
+export default Home
